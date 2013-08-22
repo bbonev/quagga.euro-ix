7d5c65317202d5dfa2ef370ca3f2d67e822b69a8
Jonathan Stout <jonstout@indiana.edu>
2013-08-22 10:32:13 -0900

    adding initial pfifo test script

---
+++ tests/test-pfifo.c
@@ -0,0 +1,10 @@
+#include "misc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "pfifo.h"
+
+int
+main(int argc, char* argv[]) {
+  fprintf(stderr, "Starting pfifo Tests\n");
+}
