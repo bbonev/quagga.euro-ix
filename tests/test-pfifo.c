bfbc1967e8f56587f4e9515afdf38e74d86c4e2a
Jonathan Stout <jonstout@indiana.edu>
2013-09-05 11:43:26 -0900

    adding and removing info from a pfifo

--- tests/test-pfifo.c
+++ tests/test-pfifo.c
@@ -5,6 +5,12 @@
 #include "pfifo.h"
 
 
+typedef struct
+{
+  struct dl_list_pair(struct datum*) list_pointers;//pfifo_pair_t list_pointers;
+  const char* item;
+} datum;
+
 static void
 pfifo_next_item(void)
 {
@@ -14,32 +20,33 @@ pfifo_next_item(void)
   max_periods = 5;
   uint alloc_on_progression;
   alloc_on_progression = 1;
-  uint pair_offset;
-  pair_offset = 2;
 
   pfifo p;
-  p = pfifo_init_new(NULL, max_periods, alloc_on_progression, pair_offset);
+  p = pfifo_init_new(NULL, max_periods, alloc_on_progression, offsetof(datum, list_pointers));
 
   /* Get current period. */
   pfifo_period_t ptime;
-  ptime = pfifo_first_period(p);
-  fprintf(stderr, "Got period: %d.\n", ptime);
+  ptime = 1;
+  fprintf(stderr, "Using period: %d.\n", (int)ptime);
 
-  /* Add items to p. */
-  char* new_stuff = "Junk Data Here";
-  pfifo_item_add( p, (void*)new_stuff, pfifo_first_period(p) );
+  /* Add items to the first period in the pfifo. */
+  datum new_stuff;
+  //new_stuff = XCALLOC(1, sizeof(datum));//= {(pfifo_pair_t) NULL, NULL };
+  new_stuff.item = "Junk Data Here";
+  fprintf(stderr, "Expected: %s\n", new_stuff.item);
+
+  pfifo_item_add( p, &new_stuff, ptime );
   fprintf(stderr, "Added item to period.\n");
 
   /* Grab the next item. */
-  void* old_stuff;
-  old_stuff = pfifo_item_next(p);
-  fprintf(stderr, "Got item from pfifo.\n");
+  datum* old_item;
+  old_item = pfifo_item_head(p);
 
-  if ( strcmp(new_stuff, (char*)old_stuff) != 0 ) {
+  if ( strcmp(new_stuff.item, old_item->item) != 0 ) {
     /* Free up any lingering memory pieces. */
-    pfifo_free(p);
     fprintf(stderr, "Failed\n");
-    fprintf(stderr, "Expected: %s, Got: %s\n", new_stuff, (char*)old_stuff);
+    fprintf(stderr, "Expected: %s, Got: %s\n", new_stuff.item, old_item->item);
+    pfifo_free(p);
   }
 
   /* Free up any lingering memory pieces. */
