77427ed8bd477edb0443faf90fff1d21f7d3a9a6
Jonathan Stout <jonstout@indiana.edu>
2013-09-13 06:58:35 -0900

    adding two testcases

--- tests/test-pfifo.c
+++ tests/test-pfifo.c
@@ -12,9 +12,8 @@ typedef struct
 } datum;
 
 static void
-pfifo_next_item(void)
+test_pfifo_item_head(void)
 {
-  fprintf(stderr, "pfifo_next_item...\t");
   /* Initialize pfifo. */
   uint max_periods;
   max_periods = 5;
@@ -24,34 +23,68 @@ pfifo_next_item(void)
   pfifo p;
   p = pfifo_init_new(NULL, max_periods, alloc_on_progression, offsetof(datum, list_pointers));
 
-  /* Get current period. */
+
+  // [BEGIN] Single item && Single period
+  //
+  // Add an item to period ptime and remove from pfifo using
+  // pfifo_item_head
+  fprintf(stderr, "test_item_head_one_period... \t");
   pfifo_period_t ptime;
   ptime = 1;
-  fprintf(stderr, "Using period: %d.\n", (int)ptime);
 
   /* Add items to the first period in the pfifo. */
   datum new_stuff;
-  //new_stuff = XCALLOC(1, sizeof(datum));//= {(pfifo_pair_t) NULL, NULL };
   new_stuff.item = "Junk Data Here";
-  fprintf(stderr, "Expected: %s\n", new_stuff.item);
+  pfifo_item_add(p, &new_stuff, ptime);
 
-  pfifo_item_add( p, &new_stuff, ptime );
-  fprintf(stderr, "Added item to period.\n");
-
-  /* Grab the next item. */
+  /* Extract item from pfifo. */
   datum* old_item;
   old_item = pfifo_item_head(p);
 
+  /* Ensure data was preserved. */
   if ( strcmp(new_stuff.item, old_item->item) != 0 ) {
-    /* Free up any lingering memory pieces. */
-    fprintf(stderr, "Failed\n");
+    fprintf(stderr, "Failed single_item_single_period\n");
     fprintf(stderr, "Expected: %s, Got: %s\n", new_stuff.item, old_item->item);
-    pfifo_free(p);
   }
+  fprintf(stderr, "OK\n");
+  //
+  // [END] Single item && Single period
+
+
+  // [BEGIN] Multiple items && Single period
+  //
+  // Add two items to two separate periods. Ensure
+  // pfifo_item_head removes both items from pfifo.
+  fprintf(stderr, "test_item_head_many_periods... \t");
+  ptime = 2;
+  datum misp_a;
+  misp_a.item = "Item a";
+  pfifo_item_add(p, &misp_a, ptime);
+
+  ptime = 3;
+  datum misp_b;
+  misp_b.item = "Item b";
+  pfifo_item_add(p, &misp_b, ptime);
+
+  datum* misp_a_out;
+  misp_a_out = pfifo_item_head(p);
+  if ( strcmp(new_stuff.item, old_item->item) != 0 ) {
+    fprintf(stderr, "Failed multiple_item_separte_period\n");
+    fprintf(stderr, "Expected: %s, Got: %s\n", misp_a.item, misp_a_out->item);
+  }
+
+  datum* misp_b_out;
+  misp_b_out = pfifo_item_head(p);
+  if ( strcmp(new_stuff.item, old_item->item) != 0 ) {
+    fprintf(stderr, "Failed multiple_item_separate_period\n");
+    fprintf(stderr, "Expected: %s, Got: %s\n", misp_b.item, misp_b_out->item);
+  }
+  fprintf(stderr, "OK\n");
+  //
+  // [END] Multiple items && Single period
 
   /* Free up any lingering memory pieces. */
   pfifo_free(p);
-  fprintf(stderr, "OK\n");
   return;
 }
 
@@ -59,5 +92,5 @@ pfifo_next_item(void)
 int
 main(int argc, char* argv[]) {
   fprintf(stderr, "Starting pfifo Tests\n");
-  pfifo_next_item();
+  test_pfifo_item_head();
 }
