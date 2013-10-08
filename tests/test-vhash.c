6df1ac0ca4835b433f37e5ba54003d3def8f62db
Jonathan Stout <jonstout@indiana.edu>
2013-10-08 10:52:18 -0900

    adding initial param funcs

--- tests/test-vhash.c
+++ tests/test-vhash.c
@@ -5,30 +5,47 @@
 #include "vhash.h"
 
 
-//
+// Ideal Struct? maybe...
+struct datum {
+  vhash_node node;
+  vhash_data_c key;
+  int value;
+};
+
+
 vhash_hash_t
 hash_func(vhash_data_c data) {
-
+  uint32 val;
+  val = (uint32)data;
+  val += 7;
+  return (vhash_hash_t)val;
 }
 
 int
 equal_func(vhash_item_c item, vhash_data_c data) {
-
+  datum* i;
+  i = (datum*)item;
+  if (hash_func(i.value) == hash_func(data)) {
+    return 0;
+  }
+  return -1;
 }
 
 vhash_item
 new_func(vhash_table table, vhash_data_c data) {
-
+  datum* i;
+  i.key = data;
+  return (vhash_item)i;
 }
 
 vhash_item
 free_func(vhash_item item, vhash_table table) {
-
+  return NULL
 }
 
 vhash_item
 orphan_func(vhash_item item, vhash_table table) {
-
+  return NULL
 }
 
 vhash_table
